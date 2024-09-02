2eefef9c83e0e320987038710d5750effd544fd9
Italo Monteiro Silva <131557854+italomsilva@users.noreply.github.com>
2024-09-02 10:27:23 -0300

    feat: Create Questao3.dart

---
+++ Questao3.dart
@@ -0,0 +1,12 @@
+void main() {
+  int indice = 12;
+  int soma = 0;
+  int k = 1;
+  
+  while (k < indice) {
+    k = k + 1;
+    soma = soma + k;
+  }
+  
+  print(soma); // Soma será igual a 77
+}
